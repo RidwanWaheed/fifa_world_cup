b73db272107a3f7843d5609607d487bd8e0cc7cd
ridwanWaheed <waheedridwan96@gmail.com>
2023-01-24 00:30:52 +0100

    chore: modified the get results to return data as the FE format

--- database/seeders/Result.ts
+++ database/seeders/Result.ts
@@ -1,9 +1,15 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import Result from 'App/Models/Result'
+import MatchFactory from 'Database/factories/MatchFactory'
+import ResultFactory from 'Database/factories/ResultFactory'
 
 export default class ResultSeeder extends BaseSeeder {
   public async run() {
-    await ResultSeeder.fetchResults()
+    await ResultSeeder.createResults()
+  }
+
+  public static async createResults() {
+    return await MatchFactory.with('result').with('teamOne').with('teamTwo').createMany(10)
   }
 
   public static async fetchResults() {
