89ece9d47afa8ff72c2148fa8f18f1e43f531d65
ridwanWaheed <waheedridwan96@gmail.com>
2023-01-24 10:34:10 +0100

    chore: added results to the create match `test helper`

--- app/Helpers/TestHelper.ts
+++ app/Helpers/TestHelper.ts
@@ -1,6 +1,7 @@
 import { Assert } from '@japa/assert'
 import GroupFactory from 'Database/factories/GroupFactory'
 import MatchFactory from 'Database/factories/MatchFactory'
+import ResultFactory from 'Database/factories/ResultFactory'
 
 export async function createGroupTeamsMatches(assert?: Assert) {
   const groups = await GroupFactory.with('teams', 4).createMany(8)
@@ -22,12 +23,20 @@ export async function createGroupTeamsMatches(assert?: Assert) {
       )
 
       tracker.push(currentTeam.id)
-      await Promise.all(
+      const matches = await Promise.all(
         otherGroupTeams.map((otherTeam) => {
           return MatchFactory.merge({
             groupId: group.id,
             team1: currentTeam.id,
             team2: otherTeam.id,
+          }).create()
+        })
+      )
+      // console.log(matches)
+      await Promise.all(
+        matches.map((match) => {
+          return ResultFactory.merge({
+            matchId: +match.id,
           }).create()
         })
       )
